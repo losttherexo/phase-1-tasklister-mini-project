0f8dc1ca4b3f35922c0fbede118b61b25f86a3dc
André Vargas Roo <a.vargas252@icloud.com>
2023-02-01 17:14:41 -0500

    added comments to index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let form = document.querySelector('form')
-  form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    buildToDo(e.target.new_task_description.value)
-    form.reset()
-  })
-});
+ document.addEventListener('DOMContentLoaded', () => {
+   let form = document.querySelector('form')
+   form.addEventListener('submit', (e) => {
+     e.preventDefault()
+     buildToDo(e.target.new_task_description.value   })
+ });
 
-function buildToDo(todo) {
-  let p = document.createElement('p')
-  let btn = document.createElement('button')
-  btn.addEventListener('click', handleDelete)
-  btn.textContent = 'x'
-  p.textContent = `${todo} `
-  p.appendChild(btn)
-  console.log(p)
-  document.querySelector('#list').appendChild(p)
-}
 
-handleDelete = (e) => {
-  e.target.parentNode.remove()
-}
+ //
+ function buildToDo(todo) {
+   let p = document.createElement('p')
+   let btn = document.createElement('button')
+   btn.addEventListener('click', handleDelete)
+   btn.textContent = 'x'
+   p.textContent = `${todo} `
+   p.appendChild(btn)
+   console.log(p)
+   document.querySelector('#list').appendChild(p)
+ }
+
+//How to delete an element
+ handleDelete = (e) => {
+   e.target.parentNode.remove()
+ }
+
